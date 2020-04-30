fec1e241c3ac182b3539f6bdc0c1bf5803f2be10
Glynis Mattheisen <gmattheisen@gmail.com>
2020-04-30 16:34:40 +0200

    update;

--- neo/io/igorproio.py
+++ neo/io/igorproio.py
@@ -113,7 +113,7 @@ class IgorIO(BaseIO):
             location = filesystem['root']
             for element in location:
                  if hasattr(location[element],'wave') == True:
-                     data = location[element].
+                     data = location[element]
         content = data['wave']
         if "padding" in content:
             assert content['padding'].size == 0, \
